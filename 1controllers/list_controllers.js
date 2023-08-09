09098836a7006bda636a57df9e3fc05a25147b97
minjun <9min3jun@naver.com>
2023-08-09 21:33:08 +0900

    fix : list 순서부여

--- 1controllers/list_controllers.js
+++ 1controllers/list_controllers.js
@@ -1,23 +1,23 @@
 const ListService = require('../2services/list_serivce');
 const jwt = require('jsonwebtoken');
 
-console.log('컨트롤러 진입');
+// console.log('리스트 컨트롤러 진입');
 
 class ListController {
   listService = new ListService();
 
+  //  리스트 만들기 매서드
   createList_Controller = async (req, res) => {
-    console.log('createList_Controller매서드 진입');
-    try {
-      const {BoardId, title, UserId, listOrder} = req.body;
+    // console.log('createList_Controller매서드 진입');
+    // console.log(BoardId, title);
 
-      console.log(BoardId, title);
+    try {
+      const {BoardId, title, UserId} = req.body;
 
       const {status, message} = await this.listService.createList_Service(
         BoardId,
         title,
         UserId,
-        listOrder,
       );
       return res.status(status).json({message});
     } catch (err) {
--- app.js
+++ app.js
@@ -4,20 +4,13 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 
-const userRouter = require('./routes/user_route');
-<<<<<<< HEAD
-const listRouter = require('./routes/list_route');
+const listRouter = require('./0routes/list_route');
+const boardRouter = require('./0routes/board_route');
+const userRouter = require('./0routes/user_route');
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', [userRouter, listRouter]);
-=======
-const boardRouter = require('./routes/board_route');
-
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api', [userRouter, boardRouter]);
->>>>>>> de7902c6a512876f9b98557c48a9d7bbd77e2de3
+app.use('/api', [userRouter, listRouter, boardRouter]);
 
 app.listen(PORT, () => {
   console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
