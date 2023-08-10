89eb53a1c54f3334618411165c392afece5d0bf8
minjun <9min3jun@naver.com>
2023-08-11 00:44:46 +0900

    기능 추가 + 못한 것
    1. 리스트 순서 바꾸기
    2. 리스트 순서 밀기(두 가지 경우 모두)
    3. 리스트 제거 기능 + 로그인 기능과 합치기만하면 api는 끝
    4. 프론트 구현해야함

--- 1controllers/list_controllers.js
+++ 1controllers/list_controllers.js
@@ -1,7 +1,7 @@
 const ListService = require('../2services/list_serivce');
 const jwt = require('jsonwebtoken');
 
-// console.log('리스트 컨트롤러 진입');
+console.log('리스트 컨트롤러 진입');
 
 class ListController {
   listService = new ListService();
@@ -18,6 +18,66 @@ class ListController {
         BoardId,
         title,
         UserId,
+      );
+      return res.status(status).json({message});
+    } catch (err) {
+      return {status: 400, message: err.message};
+    }
+  };
+
+  //  리스트 불러오기 매서드
+  getList_Controller = async (req, res) => {
+    try {
+      const {listId} = req.params;
+
+      const {status, message} = await this.listService.getList_Service(listId);
+      return res.status(status).json({message});
+    } catch (err) {
+      return {status: 400, message: err.message};
+    }
+  };
+
+  //  리스트 수정하기 매서드
+  putList_Controller = async (req, res) => {
+    try {
+      const {listId} = req.params;
+      const {title} = req.body;
+
+      const {status, message} = await this.listService.putList_Service(
+        listId,
+        title,
+      );
+      return res.status(status).json({message});
+    } catch (err) {
+      return {status: 400, message: err.message};
+    }
+  };
+
+  // 리스트 순서 바꾸기 매서드
+  exchangeList_Controller = async (req, res) => {
+    try {
+      const {listOrder} = req.params;
+      const {listOrderNew} = req.body;
+
+      const {status, message} = await this.listService.exchangeList_Service(
+        listOrder,
+        listOrderNew,
+      );
+      return res.status(status).json({message});
+    } catch (err) {
+      return {status: 400, message: err.message};
+    }
+  };
+
+  //  리스트 밀기 매서드
+  moveList_Controller = async (req, res) => {
+    try {
+      const {listOrder} = req.params;
+      const {listOrderNew} = req.body;
+
+      const {status, message} = await this.listService.moveList_Service(
+        listOrder,
+        listOrderNew,
       );
       return res.status(status).json({message});
     } catch (err) {
